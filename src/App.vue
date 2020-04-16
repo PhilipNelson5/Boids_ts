67b41b1e7231a70fc12dbbccb8289540a5705bd4
pnelson <philip.nelson@sdl.usu.edu>
2020-04-15 20:15:58 -0600

    adding github button

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
+    <Github class="right" url="https://github.com/PhilipNelson5/Boids_ts" />
     <Boids />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import Boids from "./components/Boids.vue";
+import Boids from "@/components/Boids.vue";
+import Github from "@/components/Github.vue";
 
 @Component({
   components: {
-    Boids,
-  },
+    Github,
+    Boids
+  }
 })
 export default class App extends Vue {}
 </script>
@@ -23,6 +26,9 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+.right {
+  float: right;
 }
 </style>
